97f3044295860792a888479f620c67eb6733425e
eriko-mahalo0917 <erikofuruichi11210812@gmail.com>
2025-10-06 23:48:08 +0900

    一旦、def get_id_elementまで

--- selenium_manager.py
+++ selenium_manager.py
@@ -10,7 +10,7 @@ class ChromeDriverManager:
     def __init__(self):
         self.getLogger = SimpleLogger()
         self.logger = self.getLogger.get_logger()
-        self.drive = self.chrome_process()
+        self.driver = self.chrome_process() #最後にここにって難しい
         
     #①設定するためのオプションを設定
     def get_chrome_options(self):
@@ -29,16 +29,44 @@ class ChromeDriverManager:
         
         #エラー時はraiseで処理を停止します
         try:
-            self.logger.info("ドライバーの起動をします")
+            self.logger.info(f"ドライバーの起動をします")
             chrome = webdriver.Chrome(service=service ,options=chrome_options)
-            self.logger.info("ドライバーが起動しました")
+            self.logger.info(f"ドライバーが起動しました")
             #この設置した設定をここで受け取ります
             return chrome
-        #起動できなかったのは愛
+        #起動できなかった場合はこれをする
         except Exception as e:
             #エラーのログを出す
             self.logger.error(f"ドライバーの起動に失敗しました")
             #失敗したerror内容をここで出す
             self.logger.error(f"エラーの内容：{e}")
             #処理を停止
+            raise
+        
+
+#＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+#--- ② ページの部品（IDやパスワードの入力欄など）を見つけるクラスを作る ---
+#WebDriverという「型」を教えるために、インポートを一つ追加　型ヒント
+from selenium.webdriver.remote.webdriver import WebDriver
+#これをインポートするをIDとか名前とか探せる
+from selenium.webdriver.common.by import By
+
+class GetElement:
+    #初期設定をする　これも共通でログを設定
+    def __init__(self,driver:WebDriver):
+        #中身はさっきと一緒だけど名前を変えた方が分かりやすいから変更
+        self.getLogger_set_up = SimpleLogger()
+        self.logger = self.getLogger_set_up.get_logger()
+        #ライブラリから持ってきて使っていて、変数へ入れ込んでいる
+        self.driver = driver
+        
+    #=====IDで要素を取得する機能==========
+    def get_id_element(self):
+        #tryの外側にinfoをおいたのは宣言自体はエラーにならないから
+        self.logger.info(f"IDで要素を取得します")
+        
+        try:
+            return self.driver.find_element(By.ID, "username")
+        except Exception as e:
+            self.logger.error(f"ID=username要素の取得に失敗しました")
             raise
