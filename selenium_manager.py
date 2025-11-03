9d564bde224407325519c3da913ceacb3865f262
eriko-mahalo0917 <erikofuruichi11210812@gmail.com>
2025-11-03 13:18:41 +0900

    修正をしてみました！

--- selenium_manager.py
+++ selenium_manager.py
@@ -1,5 +1,4 @@
-#インポート####################################################################
-
+####クラスとは再利用可能＆拡張性があること！忘れることのないように！#################
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -11,120 +10,163 @@ from selenium.webdriver.remote.webelement import WebElement
 from logger import SimpleLogger
 from chrome import ChromeManager
 
-#############################################################################
-
-#1つ目のフロー：ブラウザを立ち上げる
-#ChromeManagerのインスタンスを呼ぶ
+###########################################################################
+#１つ目のフロー
+# ブラウザを立ち上げる
 class ChromeDriverManager:
     def __init__(self):
         self.chrome_manager = ChromeManager()
-    
-    #デフォルトは1000，1000だけど、自由に設定できるようにする
+        
     def chrome_process(self, window_size = "1000,1000"):
         service = Service()
         driver = self.chrome_manager.start_chrome(window_size=window_size)
         return driver
     
-    
 
-#２つ目のフロー
-#ID、パスワードを取得
+#2つ目のフロー
+#要素を取得する
 class GetElement:
-    #探すためのdriverが必要だから、引数にいる
     def __init__(self, driver: WebDriver):
         self.getLogger_set_up = SimpleLogger()
         self.logger = self.getLogger_set_up.get_logger()
         self.driver = driver
         
-    #ID取得
-    def get_id_element(self, id_locator: str):
-        self.logger.info(f"ID='{id_locator}' で要素を取得します")
+    #IDを取得する
+    def get_element_by_ID(self, locator: str):
+        self.logger.info(f"ID='{locator}' で要素を取得します")
         try:
-            id_element = self.driver.find_element(By.ID, id_locator)
-            self.logger.info(f"ID='{id_locator}' の取得に成功しました")
+            id_element = self.driver.find_element(By.ID, locator)
+            self.logger.info(f"ID='{locator}' の取得に成功しました")
             return id_element
         except Exception as e:
-            self.logger.error(f"ID='{id_locator}' 要素の取得に失敗しました\nエラー内容: {e}")
+            self.logger.error(f"ID='{locator}'の取得に失敗しました\nエラー内容{e}")
             #処理停止
             raise
         
-    #パスワード取得
-    def get_pass_element(self, pass_locator: str):
-        self.logger.info(f"PASS='{pass_locator}'の取得します")
+    
+    #NAMEを取得する
+    def get_element_by_name(self, locator: str):
+        self.logger.info(f"NAME='{locator}'で要素を取得します")
         try:
-            pass_element = self.driver.find_element(By.ID, pass_locator)
-            self.logger.info(f"PASS='{pass_locator}'の取得に成功しました")
-            return pass_element
+            name_element = self.driver.find_element(By.NAME, locator)
+            self.logger.info(f"NAME='{locator}' の取得に成功しました")
+            return name_element
         except Exception as e:
-            self.logger.error(f"PASS='{pass_locator}'の要素の取得に失敗しました\nエラー内容：{e}")
+            self.logger.error(f"NAME='{locator}' の取得に失敗しました\nエラー内容{e}")
             #処理停止
             raise
         
         
-    #チェックボックス探す
-    def get_check_box_element(self, check_locator: str):
-        self.logger.info(f"チェックボックス=’{check_locator}’の要素を取得します")
+    #XPATHで取得する
+    def get_element_by_xpath(self, locator: str):
+        self.logger.info(f"XPath='{locator}' で要素を取得します")
         try:
-            check_element = self.driver.find_element(By.ID, check_locator)
-            self.logger.info(f"チェックボックス='{check_locator}' の取得に成功しました")
-            return check_element
+            xpath_element = self.driver.find_element(By.XPATH, locator)
+            self.logger.info(f"XPath='{locator}' の取得に成功しました")
+            return xpath_element
         except Exception as e:
-            self.logger.error(f"チェックボックス='{check_locator}' 要素の取得に失敗しました\nエラー内容: {e}")
+            self.logger.error(f"XPath='{locator}' 要素の取得に失敗しました\nエラー内容: {e}")
             #処理停止
             raise
         
-    #ログインボタン探す
-    def get_login_btn_element(self, login_btn_locator: str):
-        self.logger.info(f"ログインボタン='{login_btn_locator}' で要素を取得します")
+        
+    #CSS_SELECTORで取得
+    def get_element_by_css_selector(self, locator: str):
+        self.logger.info(f"CSS_SELECTOR='{locator}' で要素を取得します")
         try:
-            btn_element = self.driver.find_element(By.ID, login_btn_locator)
-            self.logger.info(f"ログインボタン='{login_btn_locator}' の取得に成功しました")
-            return btn_element
+            css_selector_element = self.driver.find_element(By.CSS_SELECTOR, locator)
+            self.logger.info(f"CSS_SELECTOR='{locator}' で取得に成功しました")
+            return css_selector_element
         except Exception as e:
-            self.logger.error(f"ログインボタン='{login_btn_locator}' の取得に失敗しました。\nエラー内容{e}")
+            self.logger.error(f"CSS_SELECTOR='{locator}' で要素に失敗しました")
             #処理停止
             raise
         
         
-            
+    #TAG_NAMEを取得する
+    def get_element_by_tag_name(self, locator: str):
+        self.logger.info(f"TAG_NAME='{locator}' で要素を取得します")
+        try:
+            tag_name_element = self.driver.find_element(By.TAG_NAME, locator)
+            self.logger.info(f"TAG_NAME='{locator}' で取得に成功しました")
+            return tag_name_element
+        except Exception as e:
+            self.logger.error(f"TAG_NAME='{locator}' で取得に失敗しました")
+            #処理停止
+            raise
+    
+    
+    #LINK_TEXTを取得する
+    def get_by_link_text(self, locator: str):
+        self.logger.info(f"LINK_NAME='{locator}' で要素を取得します")
+        try:
+            link_text_element = self.driver.find_element(By.LINK_TEXT, locator)
+            self.logger.info(f"LINK_NAME='{locator}' で取得に成功しました")
+            return link_text_element
+        except Exception as e:
+            self.logger.error(f"LINK_NAME='{locator}' で取得に失敗しました")
+            #処理停止
+            raise
+        
+    
+    #CLASS_NAMEを取得する
+    def get_element_by_class_name(self, locator: str):
+        self.logger.info(f"CLASS_NAME='{locator}' で要素を取得します")
+        try:
+            class_name_element = self.driver.find_element(By.CLASS_NAME, locator)
+            self.logger.info(f"CLASS_NAME='{locator}' で取得に成功しました")
+            return class_name_element
+        except Exception as e:
+            self.logger.error(f"CLASS_NAME='{locator}' で取得に失敗しました")
+            #処理停止
+            raise
+        
+        
 #３つ目のフロー
-#操作してログインボタンをクリック＋自分で見つける　※※ここでGetElementを呼び出して使う※※
+##操作してログインボタンをクリック＋自分で見つける　※※ここでGetElementを呼び出して使う※※
 class ActionElement:
     def __init__(self,get_element_instance: GetElement):
         self.getLogger_set_up = SimpleLogger()
         self.logger = self.getLogger_set_up.get_logger()
-        self.get_element = get_element_instance
+        self.get_element_instance = get_element_instance
         
+    
+    #click_clear_inputとclick_elementで取得する共通部分
+    def get_element(self, method_name: str, locator: str):
+        self.logger.info("要素を取得します")
+        get_method = getattr(self.get_element_instance, method_name)
+        element =   get_method(locator)
+        self.logger.info("要素を取得しました")
+        return element
+    
         
-    #クリアしてから入力する 
-    def click_clear_input(self, locator: str,input_text: str):
-        #GetElementを利用してクリック・クリアを入力する
-        self.logger.info(f"{locator}にテキストを入力します。")
-        #空っぽの箱を用意
-        
+    #クリアをしてから入力する
+    def click_clear_input(self, method_name: str, locator: str, input_text: str):
+        self.logger.info("テキストを入力します。")
         try:
-            element = self.get_element.get_id_element(locator)
-            #クリックをして入力できるようにする
+            #共通の呼び出す
+            element = self.get_element(method_name, locator)
+            #操作する部分
             element.click()
-            #一旦、クリアする
             element.clear()
-            #テキスト入力
             element.send_keys(input_text)
-            self.logger.info(f"テキスト入力に成功しました")
+            self.logger.info(f"{method_name}で取得した要素に '{input_text}' を入力しました")
         except Exception as e:
-            self.logger.error(f"テキスト入力に失敗しました。\nエラー内容{e}")
+            self.logger.error("テキスト入力に失敗しました\ｎ{e}")
             #処理停止
             raise
         
-    #クリックする
-    def click_element(self, locator: str):
-        self.logger.info(f"{locator}の要素をクリックします")
         
+    #クリックをする
+    def click_element(self, method_name: str, locator: str):
+        self.logger.info("要素をクリックします")
         try:
-            element = self.get_element.get_login_btn_element(locator)
+            #共通の呼び出す
+            element = self.get_element(method_name, locator)
+            #操作する部分
             element.click()
-            self.logger.info(f"クリックに成功しました")
+            self.logger.info(f"{method_name}で取得した要素をクリックしました")
         except Exception as e:
-            self.logger.error(f"ID='{locator}' のクリックまたは要素検索に失敗しました。\nエラー: {e}")
+            self.logger.error(f"クリックに失敗しました\n{e}")
             #処理停止
             raise
