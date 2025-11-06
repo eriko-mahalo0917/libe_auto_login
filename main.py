0ade497c5a3271f0df51ef4d2b5ca9f2fc0ee842
eriko-mahalo0917 <erikofuruichi11210812@gmail.com>
2025-11-06 23:37:36 +0900

    flow.pyに合わせて修正しました！

--- main.py
+++ main.py
@@ -1,56 +1,42 @@
-#selenium.pyからインポートしてclassを呼び出す
-from selenium_manager import ChromeDriverManager, GetElement ,ActionElement
-from selenium.webdriver.common.by import By
+#importしてくる##############################################################
+from flow import AutoLoginFlow
 
-#============================================================================
-#メインで実行するためのコード
-if __name__=="__main__":
-    #seleniumのメソッドを呼び出し、インスタンスを作成
-    driver_manager = ChromeDriverManager()
-    
-    #①ブラウザを起動して、サイズの指定も可能
-    print("ブラウザを起動します...🤖")
-    driver = driver_manager.chrome_process(window_size="1500,1000")
-    
-    
-    #②driverにこのサイトを開いてと命令する
-    aeon_url = "https://www.aeon-kyushu.com/signin"
-    #selenium_managerのここdef chrome_process(self,window_size="1000,1000"):
-    driver.get(url = aeon_url)
-    print(f"{aeon_url} を開きました！")
-    
-    
-    #③ページとID入力欄などを探す専門家「GetElement」を呼ぶ
-    #①で作った「driver」を渡す
-    #get_element = GetElement(driver=driver)
-    
-    
-    #④操作をする専門家「ActionElement」を呼ぶ
-    action_element = ActionElement()
-    
-    #⑤ID入力欄を探してとお願いし、見つけた部品を変数に入れる　※本当のIDの名前
-    id_box = driver.find_element(By.NAME, "LOGIN_ID")
-    #見つけたID入力欄に文字を入力してとお願いする
-    action_element.input_text(element=id_box, text = "eriko.1024@icloud.com")
-    print("IDを入力しました！👑")
-    
-    
-    #⑥パスワード入力欄を探してと命令する
-    pass_box = driver.find_element(By.NAME ,"PASSWORD")
-    #見つけたパスワード入力欄に文字を入力してとお願いする
-    action_element.input_text(element=pass_box, text="pppppppppp")
-    print("パスワードを入力しました。")
-    
-    #ログインボタンを探して
-    login_btn = driver.find_element(By.CLASS_NAME, "btn-submit")
-    #見つけたログインボタンをクリックしてほしいとお願いする
-    action_element.click_element(element=login_btn)
-    print("ログインボタンをクリックしました！🎉\nログイン成功！")
-    
-    
-    #ログイン後、ユーザーがエンターを押すまでまつ
-    input("ブラウザを閉じるにはエンターを教えてください：")
-    
-    #閉じる作業を入れる
-    driver.quit()
-    print("ブラウザを閉じました。")
+###########################################################################
+#クラスを作成＋インスタンス化
+class Main:
+    def __init__(self):
+        self.auto_login_flow = AutoLoginFlow()
+        
+    def main(self):
+        
+        #リベのログインサイト
+        libe_url = "https://libecity.com/signin"
+        
+        #IDの情報
+        id_method = "find_by_css_selector"
+        id_value = "#contents_wrap > main > div.login_tab_box.is_show > section > div:nth-child(1) > p.form_data_area > input[type=text]"
+        
+        #PASSの情報
+        pass_method = "find_by_css_selector"
+        pass_value = "input[type='password']"
+        
+        #ログインボタンの情報
+        login_btn_method = "find_by_xpath"
+        login_btn_value = '//*[@id="contents_wrap"]/main/div[1]/section/p[1]/button'
+        
+        self.auto_login_flow.process(
+            url=libe_url,
+            id_method = id_method,
+            id_value = id_value,
+            pass_method = pass_method,
+            pass_value = pass_value,
+            login_btn_method =login_btn_method,
+            login_btn_value = login_btn_value
+        )
+            
+            
+#＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+if __name__ == "__main__":
+    main_instance = Main()
+    main_instance.main()
+    print("テストが完了しました。")
